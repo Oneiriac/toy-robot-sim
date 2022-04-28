9126811b6e558f59e3c657bb17f3bbc46ca3bd05
Damon Cai <20635117+Oneiriac@users.noreply.github.com>
2022-04-29 00:41:14 +1000

    Add additional test cases for CLI

--- test/commands/run.test.ts
+++ test/commands/run.test.ts
@@ -1,16 +1,41 @@
 import { expect, test } from "@oclif/test";
 
 describe("run (from file input)", () => {
+    const expectEqualWithNewline = (
+        ctx: any,
+        expected: string
+    ): Chai.Assertion => expect(ctx.stdout).to.equal(`${expected}\n`);
+
     test.stdout()
         .command(["run", "test/inputs/example-A.txt"])
         .it("handles example A correctly", (ctx) => {
-            expect(ctx.stdout).to.equal("0,1,NORTH\n");
+            expectEqualWithNewline(ctx, "0,1,NORTH");
         });
 
     test.stdout()
         .command(["run", "test/inputs/example-B.txt"])
         .it("handles example A correctly", (ctx) => {
-            expect(ctx.stdout).to.equal("0,0,WEST\n");
+            expectEqualWithNewline(ctx, "0,0,WEST");
+        });
+    test.stdout()
+        .command(["run", "test/inputs/invalid-direction.txt"])
+        .it("ignores invalid direction", (ctx) => {
+            expectEqualWithNewline(ctx, "1,3,WEST");
+        });
+    test.stdout()
+        .command(["run", "test/inputs/invalid-numbers.txt"])
+        .it("ignores invalid numbers in coordinates", (ctx) => {
+            expectEqualWithNewline(ctx, "1,3,WEST");
+        });
+    test.stdout()
+        .command(["run", "test/inputs/invalid-commands.txt"])
+        .it("ignores invalid commands", (ctx) => {
+            expectEqualWithNewline(ctx, "1,3,WEST");
+        });
+    test.stdout()
+        .command(["run", "test/inputs/trim-whitespace.txt"])
+        .it("trims leading and trailing whitespace", (ctx) => {
+            expectEqualWithNewline(ctx, "4,4,EAST");
         });
 });
 
